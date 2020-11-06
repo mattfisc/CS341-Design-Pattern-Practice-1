a26b025dcc72650b645f5809080b008f83aab633
Matthew Fischer <mattfisc@gmail.com>
2020-11-06 03:57:28 -0800

    docstring added

--- src/ListItem.java
+++ src/ListItem.java
@@ -2,23 +2,46 @@
 
 import javax.swing.Icon;
 
+/**
+ * 
+ * @author Matthew Fischer
+ *
+ */
 public class ListItem implements Comparable<ListItem> {
 	private int valA;
 	private int valB;
 	
+	/**
+	 * ListItem constructor
+	 * @param valA
+	 * @param valB
+	 */
 	public ListItem(int valA, int valB) {
 		this.valA = valA;
 		this.valB = valB;
 	}
 
+	/**
+	 * getValA function gets an int value A
+	 * @return valA int
+	 */
 	public int getValA() {
 		return valA;
 	}
 	
+	/**
+	 * getValB function gets an int value B
+	 * @return valB int
+	 */
 	public int getValB() {
 		return valB;
 	}
 
+	/**
+	 * createIcon function converts a ListItem to an Icon
+	 * @param diameter int is the diameter of the Icon
+	 * @return Icon of the ListItem
+	 */
 	public Icon createIcon(int diameter) {
 		return new ItemIcon(this, diameter);
 	}
